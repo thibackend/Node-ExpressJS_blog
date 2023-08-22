4ef6a7145df04597ce88712346d42e5e7ced5acb
thibackend <thi.a24technology@gmail.com>
2023-08-22 09:06:22 +0700

    husky lint-staged prettier

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.engine(
         ],
     }),
 );
-app.set("view engine", 'hbs');
+app.set('view engine', 'hbs');
 app.set('views', [
     path.join(__dirname, '/resources/views'),
     path.join(__dirname, '/resources/views/pages'),
